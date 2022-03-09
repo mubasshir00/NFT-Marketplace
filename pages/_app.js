fd6b7b6d66dd54a8f94a1175b9220cf2f09d96fc
Mubasshir Ahmed <30567773+mubasshir00@users.noreply.github.com>
2022-03-09 18:59:31 +0600

    coinbase

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,35 @@
 import '../styles/globals.css'
 import Link from 'next/link'
+import { Web3ReactProvider } from '@web3-react/core'
+import {Web3Provider} from "@ethersproject/providers"
+import { WalletLinkConnector } from "@web3-react/walletlink-connector";
+import { WalletConnectConnector } from "@web3-react/walletconnect-connector";
+import { InjectedConnector } from "@web3-react/injected-connector";
+import { useWeb3React } from '@web3-react/core'
+
+function getLibrary(provider){
+  return new Web3Provider(provider)
+}
+
+const CoinbaseWallet = new WalletLinkConnector({
+  url: `https://mainnet.infura.io/v3/${process.env.INFURA_KEY}`,
+  appName:"nft-marketplace",
+  supportedChainIds:[1,2,3,5,42],
+});
+
+const WalletConnect = new WalletConnectConnector({
+  rpcUrl: `https://mainnet.infura.io/v3/14a516fe324b4f40a57212bf4eae56c2`,
+  bridge: "https://bridge.walletconnect.org",
+  qrcode: true,
+})
+
+const injected = new InjectedConnector({
+  supportedChainIds:[1,3,4,5,42]
+})
 
 function MyApp({ Component, pageProps }) {
+  const { activate, deactivate } = useWeb3React();
+
   return (
     <div>
       <nav className="border-b p-6">
@@ -33,6 +61,10 @@ function MyApp({ Component, pageProps }) {
             </a>
           </Link>
         </div>
+        <button className="mr-6 text-red-500" onClick={() => { activate(CoinbaseWallet) }}>Coinbase Wallet</button>
+        <button className="mr-6 text-red-500" onClick={() => { activate(WalletConnect) }}>Wallet Connect</button>
+        <button className="mr-6 text-red-500" onClick={() => { activate(Injected) }}>Metamask</button>
+        <button className="mr-6 text-red-500" onClick={deactivate}>Disconnect</button>
       </nav>
       <Component {...pageProps} />
     </div>
